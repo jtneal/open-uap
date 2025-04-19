dafd71e6075afb78ee9e8f9b912511dcc3aaff9e
Jason Neal <88398073+jneal-afs@users.noreply.github.com>
2025-04-18 21:34:48 -0700

    Fix up module boundaries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,10 +17,34 @@ export default [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'scope:web',
+              onlyDependOnLibsWithTags: ['scope:web', 'scope:common'],
             },
-          ],
+            {
+              sourceTag: 'scope:api',
+              onlyDependOnLibsWithTags: ['scope:api', 'scope:common'],
+            },
+            {
+              sourceTag: 'scope:common',
+              onlyDependOnLibsWithTags: ['scope:common'],
+            },
+            {
+              sourceTag: 'type:feature',
+              onlyDependOnLibsWithTags: ['type:feature', 'type:data-access', 'type:ui', 'type:util'],
+            },
+            {
+              sourceTag: "type:data-access",
+              onlyDependOnLibsWithTags: ["type:data-access", "type:util"]
+            },
+            {
+              sourceTag: "type:ui",
+              onlyDependOnLibsWithTags: ["type:ui", "type:util"]
+            },
+            {
+              sourceTag: "type:util",
+              onlyDependOnLibsWithTags: ["type:util"]
+            }
+          ]
         },
       ],
     },
